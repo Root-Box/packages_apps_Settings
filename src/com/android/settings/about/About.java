d72f381805072265818be76e45f35165caeb4a40
Bajee11 <bajee11@gmail.com>
2012-10-22 00:10:45 -0400

    Include About RootBox

---
+++ src/com/android/settings/about/About.java
@@ -0,0 +1,68 @@
+package com.android.settings.about;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.about.AOKPPreferenceFragment;
+import com.aokp.romcontrol.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class About extends AOKPPreferenceFragment {
+
+    public static final String TAG = "About";
+
+    Preference mSiteUrl;
+    Preference mSourceUrl;
+    Preference mReviewUrl;
+    Preference mIrcUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.title_about);
+        addPreferencesFromResource(R.xml.prefs_about);
+        mSiteUrl = findPreference("rootbox_website");
+        mSourceUrl = findPreference("rootbox_source");
+        mIrcUrl = findPreference("rootbox_irc");
+
+        PreferenceGroup devsGroup = (PreferenceGroup) findPreference("devs");
+        ArrayList<Preference> devs = new ArrayList<Preference>();
+        for (int i = 0; i < devsGroup.getPreferenceCount(); i++) {
+            devs.add(devsGroup.getPreference(i));
+        }
+        devsGroup.removeAll();
+        devsGroup.setOrderingAsAdded(false);
+        Collections.shuffle(devs);
+        for(int i = 0; i < devs.size(); i++) {
+            Preference p = devs.get(i);
+            p.setOrder(i);
+
+            devsGroup.addPreference(p);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mSiteUrl) {
+            launchUrl("http://wheretwo.com/Rootbox");
+        } else if (preference == mSourceUrl) {
+            launchUrl("http://github.com/Root-Box");
+        } else if (preference == mIrcUrl) {
+            launchUrl("http://webchat.freenode.net/?channels=aokp-sgs2");
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(donate);
+    }
+}
