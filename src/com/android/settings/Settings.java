b34e5fc8024dc70b5ba83af441ff6f8a5adc0188
Bajee11 <bajee11@gmail.com>
2013-02-20 22:44:49 -0500

    Move profiles under Personal header

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -860,6 +860,7 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
+    public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class AnonymousStatsActivity extends Settings { /* empty */ }
     public static class AboutActivity extends Settings { /* empty */ }
     public static class RootboxActivity extends Settings { /* empty */ }
