4568fb753994a718e70e12434b639db35a6ea18a
Bajee11 <bajee11@gmail.com>
2013-03-21 19:45:16 -0400

    Revert "Dark RootBox: Restart SystemUI upon switch"
    This reverts commit 8a37266f7d7b850e470f2f971701315a9576eec6.

--- src/com/android/settings/slim/TRDSEnabler.java
+++ src/com/android/settings/slim/TRDSEnabler.java
@@ -23,7 +23,6 @@ import android.content.IntentFilter;
 import android.provider.Settings;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import com.android.settings.util.Helpers;
 
 public class TRDSEnabler implements CompoundButton.OnCheckedChangeListener {
     private final Context mContext;
@@ -67,7 +66,6 @@ public class TRDSEnabler implements CompoundButton.OnCheckedChangeListener {
         // Handle a switch change
         Settings.Secure.putInt(mContext.getContentResolver(),
                 Settings.Secure.UI_INVERTED_MODE, isChecked ? 1 : 0);
-        Helpers.restartSystemUI();
     }
 
 }
