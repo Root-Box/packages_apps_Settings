609714dc5cd7971ca42be222eafdea367f4f363d
Ryan <wallace.ryan10@gmail.com>
2013-06-25 15:38:57 -0300

    Revert Security Settings to previous Commit
    2f2832b5f2249a4848784e92dd107acf390f4b69

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -408,13 +408,13 @@ public class SecuritySettings extends SettingsPreferenceFragment
             updateSmsSecuritySummary(smsSecurityCheck);
 
             // App security settings
-            //  addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
-            //  mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
-            //  if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
-            //    mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
-            //    mSmsSecurityCheck.setOnPreferenceChangeListener(this);
-            //    int smsSecurityCheck = Integer.valueOf(mSmsSecurityCheck.getValue());
-            //    updateSmsSecuritySummary(smsSecurityCheck);
+            addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
+            mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
+            if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+                mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
+                mSmsSecurityCheck.setOnPreferenceChangeListener(this);
+                //int smsSecurityCheck = Integer.valueOf(mSmsSecurityCheck.getValue());
+                //updateSmsSecuritySummary(smsSecurityCheck);
             } else {
                 PreferenceGroup appCategory = (PreferenceGroup)
                         root.findPreference(KEY_APP_SECURITY_CATEGORY);
