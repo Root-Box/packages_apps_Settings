900587fa2021812060597c039fe24e2a737a7b68
Bajee11 <bajee11@gmail.com>
2013-08-01 12:48:26 -0400

    About RB: Goodbye twitter, Welcome Gravatar.

--- src/com/android/settings/widgets/DeveloperPreference.java
+++ src/com/android/settings/widgets/DeveloperPreference.java
@@ -3,6 +3,7 @@ package com.android.settings.widgets;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Point;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -12,10 +13,12 @@ import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.preference.Preference;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -32,10 +35,14 @@ import org.apache.http.client.params.HttpClientParams;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 
-public class DeveloperPreference extends Preference {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+public class DeveloperPreference extends Preference {
     private static final String TAG = "DeveloperPreference";
 
+    public static final String GRAVATAR_API = "http://www.gravatar.com/avatar/";
+    public static int mDefaultAvatarSize = 200;
     private ImageView twitterButton;
     private ImageView donateButton;
     private ImageView githubButton;
@@ -51,26 +58,35 @@ public class DeveloperPreference extends Preference {
     private String githubLink;
     private String googleLink;
     private String facebookLink;
+    private String devEmail;
+    private final Display mDisplay;
 
     public DeveloperPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DeveloperPreference);
-        nameDev = a.getString(R.styleable.DeveloperPreference_nameDev);
-        twitterName = a.getString(R.styleable.DeveloperPreference_twitterHandle);
-        donateLink = a.getString(R.styleable.DeveloperPreference_donateLink);
-        githubLink = a.getString(R.styleable.DeveloperPreference_githubLink);
-        googleLink = a.getString(R.styleable.DeveloperPreference_googleLink);
-        facebookLink = a.getString(R.styleable.DeveloperPreference_facebookLink);
-        a.recycle();
+        TypedArray typedArray = null;
+        try {
+            typedArray = context.obtainStyledAttributes(attrs, R.styleable.DeveloperPreference);
+            nameDev = typedArray.getString(R.styleable.DeveloperPreference_nameDev);
+            twitterName = typedArray.getString(R.styleable.DeveloperPreference_twitterHandle);
+            donateLink = typedArray.getString(R.styleable.DeveloperPreference_donateLink);
+            githubLink = typedArray.getString(R.styleable.DeveloperPreference_githubLink);
+            devEmail = typedArray.getString(R.styleable.DeveloperPreference_emailDev);
+            googleLink = typedArray.getString(R.styleable.DeveloperPreference_googleLink);
+            facebookLink = typedArray.getString(R.styleable.DeveloperPreference_facebookLink);
+        } finally {
+            if (typedArray != null) {
+                typedArray.recycle();
+            }
+        }
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        mDisplay = wm.getDefaultDisplay();
     }
 
     @Override
     protected View onCreateView(ViewGroup parent) {
         super.onCreateView(parent);
-
         View layout = View.inflate(getContext(), R.layout.dev_card, null);
-
         twitterButton = (ImageView) layout.findViewById(R.id.twitter_button);
         donateButton = (ImageView) layout.findViewById(R.id.donate_button);
         githubButton = (ImageView) layout.findViewById(R.id.github_button);
@@ -78,14 +94,12 @@ public class DeveloperPreference extends Preference {
         facebookButton = (ImageView) layout.findViewById(R.id.facebook_button);
         devName = (TextView) layout.findViewById(R.id.name);
         photoView = (ImageView) layout.findViewById(R.id.photo);
-
         return layout;
     }
 
     @Override
     protected void onBindView(View view) {
         super.onBindView(view);
-
         if (donateLink != null) {
             final OnClickListener openDonate = new OnClickListener() {
                 @Override
@@ -96,7 +110,6 @@ public class DeveloperPreference extends Preference {
                     getContext().startActivity(intent);
                 }
             };
-
             donateButton.setOnClickListener(openDonate);
         } else {
             donateButton.setVisibility(View.GONE);
@@ -112,7 +125,6 @@ public class DeveloperPreference extends Preference {
                     getContext().startActivity(intent);
                 }
             };
-
             githubButton.setOnClickListener(openGithub);
         } else {
             githubButton.setVisibility(View.GONE);
@@ -164,18 +176,41 @@ public class DeveloperPreference extends Preference {
 
             // changed to clicking the preference to open twitter
             // it was a hit or miss to click the twitter bird
-            // twitterButton.setOnClickListener(openTwitter);
             this.setOnPreferenceClickListener(openTwitter);
-            final String url = "http://api.twitter.com/1/users/profile_image/" + twitterName
-                    + "?size=original";
-            UrlImageViewHelper.setUrlDrawable(this.photoView, url, R.drawable.ic_null,
+            UrlImageViewHelper.setUrlDrawable(this.photoView,
+                    getGravatarUrl(devEmail),
+                    R.drawable.ic_null,
                     UrlImageViewHelper.CACHE_DURATION_ONE_WEEK);
         } else {
             twitterButton.setVisibility(View.INVISIBLE);
             photoView.setVisibility(View.GONE);
         }
-
         devName.setText(nameDev);
 
+    }
+
+    public String getGravatarUrl(String email) {
+        try {
+            Point point = new Point();
+            mDisplay.getSize(point);
+            mDefaultAvatarSize = point.x;
+            String emailMd5 = getMd5(email.trim().toLowerCase());
+            return String.format("%s%s?s=%d",
+                    GRAVATAR_API,
+                    emailMd5,
+                    mDefaultAvatarSize);
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    private String getMd5(String devEmail) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(devEmail.getBytes());
+        byte byteData[] = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++)
+            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        return sb.toString();
     }
 }
